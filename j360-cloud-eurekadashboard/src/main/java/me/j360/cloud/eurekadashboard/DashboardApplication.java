246c7d86e74dfa5dfd4057b7964441f432ad37f2
reedF <reed.fym@gmail.com>
2016-01-29 17:31:16 +0800

    comment

--- j360-cloud-eurekadashboard/src/main/java/me/j360/cloud/eurekadashboard/DashboardApplication.java
+++ j360-cloud-eurekadashboard/src/main/java/me/j360/cloud/eurekadashboard/DashboardApplication.java
@@ -5,7 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
-
+/**
+ * 1.通过http://localhost:8989/turbine.stream查看集群状态时，
+ * 需有对http://localhost:8080/hytrix的连续访问才会有数据
+ * 2.通过http://localhost:8989/hystrix.stream查询单个节点状态
+ *
+ */
 @SpringBootApplication
 @EnableEurekaClient
 @EnableHystrixDashboard
